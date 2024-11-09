b3228cbea4da78119d4f3f37e89064597fc9099d
Xuan <97ssps30212@gmail.com>
2024-11-09 21:52:32 +0800

    support decode html

--- crates/svelte-parser/src/ast/text.rs
+++ crates/svelte-parser/src/ast/text.rs
@@ -1,10 +1,12 @@
+use std::borrow::Cow;
+
 use crate::Parser;
 use oxc_span::Span;
 
 #[derive(Debug, Clone)]
 pub struct Text<'a> {
     pub span: Span,
-    pub data: String,
+    pub data: Cow<'a, str>,
     pub raw: &'a str,
 }
 
@@ -18,12 +20,10 @@ impl<'a> Parser<'a> {
             self.next();
         }
         let raw = &self.source[start..self.offset_u()];
-        // TODO: data should be decoded
-        let data = raw.to_string();
         let text = Text {
             span: Span::new(start as u32, self.offset),
             raw,
-            data,
+            data: htmlize::unescape(raw),
         };
 
         text
