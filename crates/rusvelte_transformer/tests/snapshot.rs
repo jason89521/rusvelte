ea5d6a5950c6bcd72fc7bc8dea3d0787a9e2ff15
Xuan <97ssps30212@gmail.com>
2024-12-04 13:02:02 +0800

    refactor: change the snapshot behaviour (#36)

--- crates/rusvelte_transformer/tests/snapshot.rs
+++ crates/rusvelte_transformer/tests/snapshot.rs
@@ -1,6 +1,7 @@
 #[test]
 fn test() {
-    insta::glob!("samples/*.svelte", |path| {
+    insta::glob!("samples/**/input.svelte", |path| {
+        let folder_path = std::path::Path::new(path).parent().unwrap();
         let source = std::fs::read_to_string(path).unwrap();
         let allocator = oxc_allocator::Allocator::default();
         let mut root = rusvelte_parser::Parser::new(&source, &allocator)
@@ -8,8 +9,10 @@ fn test() {
             .expect("Parse failed");
         let (scopes, _, symbols) = rusvelte_analyzer::Analyzer::default().analyze(&root);
         let program = rusvelte_transformer::Transformer::new(&allocator, scopes, symbols)
-            .transform(&mut root);
+            .client_transform(&mut root);
         let code = oxc_codegen::Codegen::new().build(&program).code;
-        insta::assert_snapshot!("client", code)
+        insta::with_settings!({snapshot_path => folder_path, snapshot_suffix => "", prepend_module_to_snapshot => false}, {
+            insta::assert_snapshot!("client", code)
+        })
     })
 }
